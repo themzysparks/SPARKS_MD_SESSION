3c69d587df32b5ee5f83715b44cef5efd08b1a13
Sparks <132665464+themzysparks@users.noreply.github.com>
2024-10-21 02:25:34 +0100

    Add files via upload

---
+++ qr.js
@@ -0,0 +1,45 @@
+/*
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                              ＭＹ ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ                                             // 
+//                                                                                                      // 
+//                                         Ｖ：１．０                                                    // 
+//                                                                                                      // 
+//                                                                                                      //
+//         ░██████╗██████╗░░█████╗░██████╗░██╗░░██╗░██████╗                                             //
+//        ██╔════╝██╔══██╗██╔══██╗██╔══██╗██║░██╔╝██╔════╝                                             //
+//        ╚█████╗░██████╔╝███████║██████╔╝█████═╝░╚█████╗░                                             //
+//        ░╚═══██╗██╔═══╝░██╔══██║██╔══██╗██╔═██╗░░╚═══██╗                                             //
+//        ██████╔╝██║░░░░░██║░░██║██║░░██║██║░╚██╗██████╔╝                                             //
+//        ╚═════╝░╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░                                             //
+//                                                                                                      //
+//                                                                                                      //
+//                                                                                                      //
+//                                                                                                      //
+//                                                                                                      //
+//                                                                                                      //
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+
+*
+   * @project_name : SPARKS_MD_SESSION
+   * @author : SPARKS
+   * @youtube : https://youtube.com/@cyberwithsparks
+   * @instagram : https://www.instagram.com/sparksthemzy
+   * @description : My private build made public for everyone to use with easy hosting.
+   * @version : 1.0
+   * Donation : https://paystack.com/pay/sparks_md_donation
+   
+   * I update my GitHub bots when I have time, focusing mainly on key projects.
+*
+   * Licensed under the GPL-3.0 License;
+* 
+   * Created By SPARKS.
+   * © 2024 SPARKS TECH SOLUTIONS.
+*/
+
+
+
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+
+
+const _0x3dc7bb=_0x1b41;function _0x1b41(_0x53b0a4,_0x32f1ed){const _0x3223d1=_0x3223();return _0x1b41=function(_0x1b41f1,_0x5ad3e1){_0x1b41f1=_0x1b41f1-0x12d;let _0xd5f020=_0x3223d1[_0x1b41f1];return _0xd5f020;},_0x1b41(_0x53b0a4,_0x32f1ed);}(function(_0x2da828,_0x166af2){const _0x16bf53=_0x1b41,_0x68b2c1=_0x2da828();while(!![]){try{const _0x35d29b=-parseInt(_0x16bf53(0x138))/0x1+parseInt(_0x16bf53(0x14d))/0x2+-parseInt(_0x16bf53(0x145))/0x3*(-parseInt(_0x16bf53(0x136))/0x4)+parseInt(_0x16bf53(0x137))/0x5+parseInt(_0x16bf53(0x14c))/0x6+parseInt(_0x16bf53(0x140))/0x7+-parseInt(_0x16bf53(0x135))/0x8;if(_0x35d29b===_0x166af2)break;else _0x68b2c1['push'](_0x68b2c1['shift']());}catch(_0x3651aa){_0x68b2c1['push'](_0x68b2c1['shift']());}}}(_0x3223,0xf00cc));const PastebinAPI=require(_0x3dc7bb(0x155)),pastebin=new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL'),{makeid}=require('./id'),QRCode=require(_0x3dc7bb(0x154)),express=require(_0x3dc7bb(0x147)),path=require(_0x3dc7bb(0x13a)),fs=require('fs');let router=express['Router']();const pino=require(_0x3dc7bb(0x14f)),{default:Venocyber_Tech,useMultiFileAuthState,jidNormalizedUser,Browsers,delay,makeInMemoryStore}=require(_0x3dc7bb(0x132));function _0x3223(){const _0x3030cf=['3530688PRVXBK','close','readFileSync','log','node:fs/promises','216465bshkTq','\x0a*_Pair\x20Code\x20Connected\x20by\x20CODERX_*\x0a*_Made\x20With\x20🤍_*\x0a______________________________________\x0a╔════◇\x0a║\x20*『\x20WOW\x20YOU\x20CHOOSEN\x20CODERX\x20』*\x0a║\x20_You\x20Have\x20Completed\x20the\x20First\x20Step\x20to\x20Deploy\x20a\x20Whatsapp\x20Bot._\x0a╚══════════════════════╝\x0a╔═════◇\x0a║\x20\x20『•••\x20𝗩𝗶𝘀𝗶𝘁\x20𝗙𝗼𝗿\x20𝗛𝗲𝗹𝗽\x20•••』\x0a║❒\x20*Ytube:*\x20https://youtube.com/@coderxsa\x0a║❒\x20*Owner:*\x20https://wa.me/27717311486\x0a║❒\x20*Repo:*\x20https://github.com/coderxsa\x0a║❒\x20*WaGroup:*\x20https://chat.whatsapp.com/CHBei04Af9CEyvtjprGXRK\x0a║❒\x20*Donation:*\x20https://pay.yoco.com/coderx\x0a║❒\x20*Plugins:*\x20https://github.com/coderxsa/ANITA\x0a╚══════════════════════╝\x20\x0a_____________________________________\x0a\x0a_Don\x27t\x20Forget\x20To\x20Give\x20Star\x20To\x20My\x20Repo_','express','open','json','connection.update','rmSync','7119066VgjSgc','2560542MiftQJ','sendMessage','pino','/creds.json','Desktop','exports','macOS','qrcode','pastebin-js','end','silent','toString','get','base64','@whiskeysockets/baileys','from','error','19050120bIDRUr','20RMAvFa','1939545IpwBac','355341sYjekC','./temp/','path','/temp/','output','existsSync','temp/','user'];_0x3223=function(){return _0x3030cf;};return _0x3223();}function removeFile(_0x360a12){const _0x3cc4f8=_0x3dc7bb;if(!fs[_0x3cc4f8(0x13d)](_0x360a12))return![];fs[_0x3cc4f8(0x14b)](_0x360a12,{'recursive':!![],'force':!![]});};const {readFile}=require(_0x3dc7bb(0x144));router[_0x3dc7bb(0x130)]('/',async(_0x52d9bb,_0x104e40)=>{const _0xc471e2=makeid();async function _0xffe7b8(){const _0x3672bc=_0x1b41,{state:_0x520ad7,saveCreds:_0x2eb00e}=await useMultiFileAuthState(_0x3672bc(0x139)+_0xc471e2);try{let _0x2c90aa=Venocyber_Tech({'auth':_0x520ad7,'printQRInTerminal':![],'logger':pino({'level':_0x3672bc(0x12e)}),'browser':Browsers[_0x3672bc(0x153)](_0x3672bc(0x151))});_0x2c90aa['ev']['on']('creds.update',_0x2eb00e),Qr_Code_By_Venocyber_Techr['ev']['on'](_0x3672bc(0x14a),async _0x5e6f93=>{const _0x1d02c9=_0x3672bc,{connection:_0x1c9feb,lastDisconnect:_0x40b84e,qr:_0x51633e}=_0x5e6f93;if(_0x51633e)await _0x104e40[_0x1d02c9(0x12d)](await QRCode['toBuffer'](_0x51633e));if(_0x1c9feb==_0x1d02c9(0x148)){await delay(0x1388);let _0x26ea08=fs[_0x1d02c9(0x142)](__dirname+(_0x1d02c9(0x13b)+_0xc471e2+_0x1d02c9(0x150)));await delay(0x320);let _0x15347a=Buffer[_0x1d02c9(0x133)](_0x26ea08)[_0x1d02c9(0x12f)](_0x1d02c9(0x131)),_0x2099bb=await _0x2c90aa[_0x1d02c9(0x14e)](_0x2c90aa[_0x1d02c9(0x13f)]['id'],{'text':''+_0x15347a}),_0x3cae21=_0x1d02c9(0x146);return await _0x2c90aa['sendMessage'](_0x2c90aa['user']['id'],{'text':Venocyber_MD_TEXT},{'quoted':_0x2099bb}),await delay(0x64),await _0x2c90aa['ws']['close'](),await removeFile(_0x1d02c9(0x13e)+_0xc471e2);}else _0x1c9feb===_0x1d02c9(0x141)&&_0x40b84e&&_0x40b84e[_0x1d02c9(0x134)]&&_0x40b84e[_0x1d02c9(0x134)][_0x1d02c9(0x13c)]['statusCode']!=0x191&&(await delay(0x2710),_0xffe7b8());});}catch(_0x683172){!_0x104e40['headersSent']&&await _0x104e40[_0x3672bc(0x149)]({'code':'Service\x20is\x20Currently\x20Unavailable'}),console[_0x3672bc(0x143)](_0x683172),await removeFile(_0x3672bc(0x13e)+_0xc471e2);}}return await _0xffe7b8();}),module[_0x3dc7bb(0x152)]=router;
