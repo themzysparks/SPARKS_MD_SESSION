0000efca9e850bbaa679686cf810c1f438774015
Sparks <132665464+themzysparks@users.noreply.github.com>
2024-10-21 02:18:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -42,27 +42,4 @@
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 
 
-const express = require("express");
-const app = express();
-__path = process.cwd();
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 0x1f40;
-let server = require('./qr');
-let code = require("./pair");
-require("events").EventEmitter.defaultMaxListeners = 0x1f4;
-app.use('/qr', server);
-app.use("/code", code);
-app.use("/pair", async (_0x53681e, _0xd517fe, _0x7ad908) => {
-  _0xd517fe.sendFile(__path + '/pair.html');
-});
-app.use('/', async (_0x4d905e, _0x245705, _0x4f38cd) => {
-  _0x245705.sendFile(__path + "/main.html");
-});
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  'extended': true
-}));
-app.listen(PORT, () => {
-  console.log("\nDon't Forgot To Give Star\n\n Server running on http://localhost:" + PORT);
-});
-module.exports = app;
+function _0x2370(){const _0x5188de=['body-parser','listen','3aDGQVV','/pair','urlencoded','/main.html','6173150KJTbvM','exports','1214614uQvvFw','./pair','135455pBcUNo','use','/code','/pair.html','8lWQROi','2811825CpPyNF','sendFile','EventEmitter','PORT','100989KnHHIH','171358HXTVjp','\x0aDon\x27t\x20Forgot\x20To\x20Give\x20Star\x0a\x0a\x20Server\x20running\x20on\x20http://localhost:','env','events','express','765580JnydfG','234gaFzOp'];_0x2370=function(){return _0x5188de;};return _0x2370();}const _0x40c074=_0x386c;(function(_0x1fa464,_0x4c1df9){const _0x28f9f0=_0x386c,_0x2bf58a=_0x1fa464();while(!![]){try{const _0x3a6a43=-parseInt(_0x28f9f0(0x192))/0x1+parseInt(_0x28f9f0(0x1a1))/0x2*(-parseInt(_0x28f9f0(0x19b))/0x3)+parseInt(_0x28f9f0(0x197))/0x4+parseInt(_0x28f9f0(0x1a3))/0x5+parseInt(_0x28f9f0(0x198))/0x6*(parseInt(_0x28f9f0(0x191))/0x7)+parseInt(_0x28f9f0(0x1a7))/0x8*(-parseInt(_0x28f9f0(0x1a8))/0x9)+parseInt(_0x28f9f0(0x19f))/0xa;if(_0x3a6a43===_0x4c1df9)break;else _0x2bf58a['push'](_0x2bf58a['shift']());}catch(_0x27bf98){_0x2bf58a['push'](_0x2bf58a['shift']());}}}(_0x2370,0x4b0a4));const express=require(_0x40c074(0x196)),app=express();__path=process['cwd']();const bodyParser=require(_0x40c074(0x199)),PORT=process[_0x40c074(0x194)][_0x40c074(0x1ab)]||0x1f40;function _0x386c(_0x11415c,_0x53a203){const _0x23708b=_0x2370();return _0x386c=function(_0x386cb3,_0x5f4303){_0x386cb3=_0x386cb3-0x191;let _0x4cc976=_0x23708b[_0x386cb3];return _0x4cc976;},_0x386c(_0x11415c,_0x53a203);}let server=require('./qr'),code=require(_0x40c074(0x1a2));require(_0x40c074(0x195))[_0x40c074(0x1aa)]['defaultMaxListeners']=0x1f4,app[_0x40c074(0x1a4)]('/qr',server),app[_0x40c074(0x1a4)](_0x40c074(0x1a5),code),app[_0x40c074(0x1a4)](_0x40c074(0x19c),async(_0xf3cc42,_0x40e9c3,_0x4e58b8)=>{const _0x46ca8c=_0x40c074;_0x40e9c3[_0x46ca8c(0x1a9)](__path+_0x46ca8c(0x1a6));}),app['use']('/',async(_0x531022,_0x348977,_0x2f5a28)=>{const _0x1e74c5=_0x40c074;_0x348977['sendFile'](__path+_0x1e74c5(0x19e));}),app[_0x40c074(0x1a4)](bodyParser['json']()),app['use'](bodyParser[_0x40c074(0x19d)]({'extended':!![]})),app[_0x40c074(0x19a)](PORT,()=>{const _0xc499e0=_0x40c074;console['log'](_0xc499e0(0x193)+PORT);}),module[_0x40c074(0x1a0)]=app;
